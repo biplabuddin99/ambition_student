761fc15024b65c1dcf88f7fa0eb5d0f1100014cc
biplabuddin99 <biplabuddin990@gmail.com>
2026-07-05 14:40:19 +0600

    dashboard modify

--- resources/views/dasbhoard/admin.blade.php
+++ resources/views/dasbhoard/admin.blade.php
@@ -4,7 +4,7 @@
 
 <div class="page-content py-3">
     <section class="row">
-        <div class="col-12 col-lg-9">
+        <div class="col-12 col-lg-12">
             <div class="row">
                 <div class="col-6 col-lg-3 col-md-6">
                     <div class="card">
@@ -87,7 +87,7 @@
                     </div>
                 </div>
             </div>
-            <div class="row">
+            {{-- <div class="row">
                 <div class="col-12 col-xl-4">
                     <div class="card">
                         <div class="card-header">
@@ -199,9 +199,9 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> --}}
         </div>
-        <div class="col-12 col-lg-3">
+        {{-- <div class="col-12 col-lg-3">
             <div class="card">
                 <div class="card-body py-4 px-4">
                     <div class="d-flex align-items-center">
@@ -260,7 +260,7 @@
                     <div id="chart-visitors-profile"></div>
                 </div>
             </div>
-        </div>
+        </div> --}}
     </section>
 </div>
 @endsection
